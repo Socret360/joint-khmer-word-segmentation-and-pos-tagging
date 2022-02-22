e34046de7c4fe9f869e4e70361553c9d41c31c33
Socret360 <lee.socret@gmail.com>
2022-02-22 10:23:59 +0700

    update README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# References
+- Buoy, R., Taing, N., & Kor, S. (2021). Joint Khmer Word Segmentation and Part-of-Speech Tagging Using Deep Learning. arXiv preprint arXiv:2103.16801.
+- Loem, M. (2021, May 4). Joint Khmer Word Segmentation and POS tagging. Medium. Retrieved February 22, 2022, from https://towardsdatascience.com/joint-khmer-word-segmentation-and-pos-tagging-cad650e78d30
--- network.py
+++ network.py
@@ -27,7 +27,7 @@ def Network(output_dim, embedding_dim, num_stacks, hidden_layers_dim, batch_size
     References
     ---
     - Buoy, R., Taing, N., & Kor, S. (2021). Joint Khmer Word Segmentation and Part-of-Speech Tagging Using Deep Learning. arXiv preprint arXiv:2103.16801.
-    - Loem, M. (2021, May 4). Joint Khmer Word Segmentation and POS tagging. Medium. Retrieved February 22, 2022, from https://towardsdatascience.com/joint-khmer-word-segmentation-and-pos-tagging-cad650e78d30 
+    - Loem, M. (2021, May 4). Joint Khmer Word Segmentation and POS tagging. Medium. Retrieved February 22, 2022, from https://towardsdatascience.com/joint-khmer-word-segmentation-and-pos-tagging-cad650e78d30
     """
     assert output_dim > 0, "output_dim must be larger than 0"
     assert embedding_dim > 0, "embedding_dim must be larger than 0"
