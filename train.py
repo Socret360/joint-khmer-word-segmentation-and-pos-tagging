01bc691424a2148e539e223a82c9e8540b297ff0
Socret360 <lee.socret@gmail.com>
2022-04-03 22:59:11 +0700

    remove tpu support

--- train.py
+++ train.py
@@ -46,11 +46,31 @@ model.compile(
     optimizer=Adam(learning_rate=config["training"]["learning_rate"])
 )
 
+
+def count_tfrecord_examples(
+        tfrecords_dir: str,
+) -> int:
+    """
+    Counts the total number of examples in a collection of TFRecord files.
+
+    :param tfrecords_dir: directory that is assumed to contain only TFRecord files
+    :return: the total number of examples in the collection of TFRecord files
+        found in the specified directory
+    """
+
+    count = 0
+    for file_name in os.listdir(tfrecords_dir):
+        tfrecord_path = os.path.join(tfrecords_dir, file_name)
+        count += sum(1 for _ in tf.data.TFRecordDataset(tfrecord_path))
+
+    return count
+
+
 dataset = tf.data.TFRecordDataset(args.train_set)
 dataset = dataset.map(lambda x: parse_tf_record_element(x, len(char_map), len(pos_map), config["model"]["max_sentence_length"]))
-dataset = dataset.batch(config["training"]["batch_size"])
+num_samples = sum(1 for _ in dataset)
 
-num_samples = dataset.reduce(np.int64(0), lambda x, _: x + 1)
+dataset = dataset.batch(config["training"]["batch_size"])
 
 print(num_samples)
 
